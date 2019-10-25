117dc70f1a5ea60ce49043b6ca760d78864b3046
LineG <ghanemline@gmail.com>
2019-10-25 19:21:57 -0400

    #11 parse the json files of each country and save some basic info in a local file, more work will have to be done to get the visa requierments since it is in an html format

--- server/data/advisory-ca.py
+++ server/data/advisory-ca.py
@@ -8,14 +8,23 @@ all_countries = all_countries['data']
 
 all_countries = all_countries.keys()
 
+countries_data = {}
+
 for key in all_countries:
     country_url = "https://data.international.gc.ca/travel-voyage/cta-cap-{}.json".format(key,sep='')
 
     with contextlib.closing(urllib.request.urlopen(country_url)) as url:
-         country_data = json.loads(url.read().decode())
-         country_data = country_data['data']
-         print(country_data['country-iso'])
+        country_data = json.loads(url.read().decode())
+        country_data = country_data['data']
+        country_iso = key
+        country_data = country_data['eng']
+        name = country_data['name']
+        advisory_text = country_data['advisory-text']
+        info = {'country-iso':country_iso,'name':name,'advisory-text':advisory_text}
+        countries_data[name]=info
 
+with open('advisory-ca.json', 'w') as fp:
+    json.dump(countries_data, fp)
 
 
 
