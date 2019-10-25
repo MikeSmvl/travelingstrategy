d54012e7ce5e09f5eb0ac0df1d36f6855eed9be9
Oumar96 <oumarba221296@hotmail.fr>
2019-10-25 15:12:29 -0400

    #29 #7 Testing graphql queries

---
+++ server/src/__tests__/resolvers.queries.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const path = require("path");
+const EasyGraphQLTester = require("easygraphql-tester");
+const { expect } = require("chai");
+const graphql = require("graphql");
+const schemaCode = fs.readFileSync(path.join(__dirname, "../graphql/", "schema.graphql"), "utf8");
+const books = require("../resolvers/books");
+
+
+describe("Test schema", () => {
+  let tester;
+  beforeAll(() => {
+    tester = new EasyGraphQLTester(schemaCode,books);
+  });
+describe("Queries", () => {
+    test("Query books by title and author", async () => {
+      const query = `
+        {
+          query { 
+            books { 
+              title
+              author
+            } 
+          }
+        }
+      `;
+      args="";
+      const result = await tester.graphql(query, {}, {}, args);
+      expect(result.data.books.author).to.be.eq("J.K. Rowling");
+      // First arg: false, there is no field notValidField
+      // Second arg: query to test
+      // tester.test(true, query);
+    });
+
+    test("Query books by title", async () => {
+      const query = `
+        {
+          query { 
+            books { 
+              title
+            } 
+          }
+        }
+      `;
+      args="";
+      const result = await tester.graphql(query, {}, {});
+      expect(result.data.books.title).to.be.eq("Harry Potter and the Chamber of Secrets");
+      // tester.test(false, query);
+    });
+
+    test("Query books by author", async () => {
+      const query = `
+        {
+          query { 
+            books { 
+              author
+            } 
+          }
+        }
+      `;
+      args="";
+      const result = await tester.graphql(query, {}, {});
+      expect(result.data.books.author).to.be.eq("J.K. Rowling");
+      // tester.test(false, query);
+    });
+  });
+});
