1244ab555a52f7f77a10e62fc3f57feae33761b6
LineG <ghanemline@gmail.com>
2019-10-25 18:14:14 -0400

    fixed the string format of the country url #11

--- server/data/advisory-ca.py
+++ server/data/advisory-ca.py
@@ -9,10 +9,11 @@ all_countries = all_countries['data']
 all_countries = all_countries.keys()
 
 for key in all_countries:
-    country_url = "https://data.international.gc.ca/travel-voyage/cta-cap-",key,".json",sep='')
-    with urllib.request.urlopen(country_url)
-        country_data = json.loads(country_url.read().decode())
-        #with open('advisory-ca.json') as f:
+    country_url = "https://data.international.gc.ca/travel-voyage/cta-cap-{}.json".format(key,sep='')
+    print(country_url)
+    # with urllib.request.urlopen(country_url)
+    #     country_data = json.loads(country_url.read().decode)
+
 
 
 
