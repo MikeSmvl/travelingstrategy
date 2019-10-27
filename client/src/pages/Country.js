4ff0afded518e6d39b05abcfbd99d11fc58b14a8
Mikael Samvelian <mikael.samvelian@gmail.com>
2019-10-26 21:46:31 -0400

    #26 Added gif background for country page

--- client/src/pages/Country.js
+++ client/src/pages/Country.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Navbar from '../components/Navbar/Navbar'
 import ReactFullpage from '@fullpage/react-fullpage';
+import { Card } from 'react-bootstrap/'
 import '../App.css';
 
 function Country() {
@@ -11,19 +12,58 @@ function Country() {
         //fullpage options
         licenseKey = {'CF1896AE-3B194629-99B627C1-841383E5'}
         scrollingSpeed = {1000} /* Options here */
-        sectionsColor={["#FFFFFF", "#000000"]}
+        sectionsColor={["#00b7ff", "#00fff0"]}
+        navigation
+        navigationPosition = {'left'}
+        navigationTooltips = {['Basics', 'Health & Safety', 'Money']}
+        anchors={['basics', 'health', 'money']}
 
         render={({ state, fullpageApi }) => {
           return (
             <ReactFullpage.Wrapper>
               <div className="section">
-                <p>Section 1 (welcome to fullpage.js)</p>
-                <button onClick={() => fullpageApi.moveSectionDown()}>
-                  Click me to move down
-                </button>
+                <div className="App">
+                  <div className="App-header" style={{ height: '50vh' }}>
+                    <div
+                      style={{
+                        textAlign: 'center',
+                        paddingTop: '110px'
+                      }}
+                    >
+                      <span
+                        style={{
+                          color: 'white',
+                          fontSize: '33px',
+                          fontWeight: '750'
+                        }}
+                      >
+                        Paris
+                      </span>
+                      <span
+                        style={{
+                          color: 'white',
+                          fontSize: '23px',
+                          fontWeight: '600',
+                          display: 'block'
+                        }}
+                      >
+                        France
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div style={{ padding: '20px' }}>
+                  <Card style={{ color: 'grey' }}>
+                    <Card.Body>This is some text within a card body.</Card.Body>
+                  </Card>
+                </div>
+                <p>Basics</p>
               </div>
               <div className="section">
-                <p>Section 2</p>
+                <p>Health & Safety</p>
+              </div>
+              <div className="section">
+                <p>Money</p>
               </div>
             </ReactFullpage.Wrapper>
           );
