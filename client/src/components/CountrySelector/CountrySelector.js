d34d3a7833934780025c244ea7b09b8ef4132c5e
TDDEL <tdelaportas@hotmail.com>
2019-11-02 17:16:13 -0400

    #18 #33 Added style component for autocomplete tag as well as trying to fix circle ci errors

--- client/src/components/CountrySelector/CountrySelector.js
+++ client/src/components/CountrySelector/CountrySelector.js
@@ -10,15 +10,14 @@ return(
     <h1 className="country-selector-title">Strategy Planner</h1>
     <Form>
       <Row className="justify-content-center">
-        <Col xs="1" sm="3" lg="4">
+        <Col xs="12" sm="3" lg="4">
           <Form.Label>From</Form.Label>
           <Autocomplete className="form-control"
           placeholder="Origin"
           style={{width: '90%'}}
           onPlaceSelected={(place) => {
-            console.log(place);
-          }}
-          types={[]}/>
+          console.log(place);}}
+          />
         </Col>
         <Col xs="12" sm="3" lg="4">
           <Form.Label>To</Form.Label>
@@ -26,11 +25,10 @@ return(
           placeholder="Destination"
           style={{width: '90%'}}
           onPlaceSelected={(place) => {
-            console.log(place);
-          }}
-          types={[]}/>
+            console.log(place);}}
+          />
         </Col>
-        <Col xs="auto" sm="auto" style={{display: "flex", "align-items": "flex-end"}}>
+        <Col xs="auto" sm="auto" style={{display: "flex", "alignItems": "flex-end"}}>
         <Link to="/country"><Button variant="outline-primary">Compare</Button></Link>
         </Col>
       </Row>
