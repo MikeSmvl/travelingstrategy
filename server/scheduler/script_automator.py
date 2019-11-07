94481aa5cc29ae28df7f8f4b45d6dd5e949346ad
steffan <steffan_v@hotmail.com>
2019-11-06 20:16:35 -0500

    #16 #42 script_automator succesfully sends email when it fails

--- server/scheduler/script_automator.py
+++ server/scheduler/script_automator.py
@@ -2,15 +2,23 @@ import sys
 import sqlite3
 import schedule
 import time
+
+
 sys.path.append('../data')
 from advisory_ca import save_to_canada
 from advisory_aus import save_to_australia
 from advisory_nz import save_to_newZealand
 
-schedule.every().day.do(save_to_canada)
-schedule.every().day.do(save_to_australia)
-schedule.every().day.do(save_to_newZealand)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+try:
+    schedule.every().day.do(save_to_canada)
+    schedule.every().day.do(save_to_australia)
+    schedule.every().day.do(save_to_newZealand)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+except:
+    import error_email
+        
