045cfc44b9da172874d86cd7006b33cdac07bc72
steffan <steffan_v@hotmail.com>
2019-11-09 16:01:27 -0500

    #16 #42 fixed bug and refactored automation

--- server/scheduler/script_automator.py
+++ server/scheduler/script_automator.py
@@ -9,17 +9,13 @@ from advisory_ca import save_to_canada
 from advisory_aus import save_to_australia
 from advisory_nz import save_to_newZealand
 
-try:
+def main():
     scheduler = SafeScheduler()
-    scheduler.every().day.do(save_to_canada)
-    scheduler.every().day.do(save_to_australia)
-    scheduler.every().day.do(save_to_newZealand)
+    scheduler.every().seconds.do(save_to_canada)
+    scheduler.every().seconds.do(save_to_australia)
+    scheduler.every().seconds.do(save_to_newZealand)
 
     while True:
         scheduler.run_pending()
         time.sleep(1)
-
-except :
-    import error_email
-
         
