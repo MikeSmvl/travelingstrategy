883d66dbb2f834b1fcc085110c35ab0a57b145e1
Oumar96 <oumarba221296@hotmail.fr>
2019-11-11 17:31:03 -0500

    #61 #66 Graphql function that queries the table of country of origin and returns the destination

--- server/src/resolvers/queries.js
+++ server/src/resolvers/queries.js
@@ -1,6 +1,7 @@
 const graphql = require('graphql');
 const canadaAll = require('./canadaToAll')
 const countryTable = require('./countryToAll')
+const countryToCountry = require('./countryToCountry')
 const logger = require('../logger/logger.js')
 
 logger.info("Querying with GraphQL")
@@ -9,7 +10,8 @@ var queryType = new graphql.GraphQLObjectType({
     name: 'Query',
     fields: {
         canadaAll,
-        countryTable
+        countryTable,
+        countryToCountry
     }
 });
 
