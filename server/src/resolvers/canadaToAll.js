fe748bcf66e89979592297da0c7aea2fbaea9989
Oumar96 <oumarba221296@hotmail.fr>
2019-11-11 16:54:16 -0500

    #61 #66 Creating country graphql object

--- server/src/resolvers/canadaToAll.js
+++ server/src/resolvers/canadaToAll.js
@@ -14,12 +14,13 @@ var canadaAll = {
     },
     resolve: (root, args, context, info) => {
         return new Promise((resolve, reject) => {
+            logger.info("Trying to query 'SELECT * FROM "+args.name+";'")
             db.all(`SELECT * FROM canada WHERE name='${args.name}';`, function(err, rows) {
                 if(err){
                     logger.error(err)
                     reject(err);
                 }
-                logger.info("'SELECT * FROM canada;' successfully queried")
+                logger.info("'SELECT * FROM "+args.name+";' successfully queried")
                 resolve(rows);
             });
         });
