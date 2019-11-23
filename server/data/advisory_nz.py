ae6f0f210a8acc2f2a78d9a2b10db60f79f5d8e2
unknown <tdelaportas@hotmail.com>
2019-11-23 00:31:13 -0500

    #36 Added python parser for new zealand.  Saves data to db

--- server/data/advisory_nz.py
+++ server/data/advisory_nz.py
@@ -1,20 +1,133 @@
 import sqlite3
+import requests
+import time
+import json
+from bs4 import BeautifulSoup
+import regex
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from helper_class.country_names import find_all_iso
+from helper_class.sqlite_advisories import sqlite_advisories
 
-def save_to_newZealand():
-    con  = sqlite3.connect('../countries.sqlite')
-    cur = con.cursor()
-    #should not create the table every time
-    #change in the future
-    cur.execute('DROP TABLE IF EXISTS newzealand')
-    con.commit()
-    cur.execute('CREATE TABLE newzealand (country_iso VARCHAR, name VARCHAR, advisory_text VARCHAR, visa_info VARCHAR)')
-    con.commit()
 
-    # hard coded values
 
-    cur.execute('INSERT INTO newzealand (country_iso,name,advisory_text,visa_info) values("CA","Canada","Exercise normal precautions","Tourist Visa not required")')
-    con.commit()
 
-    con.close()
 
-save_to_newZealand()
+def get_url_of_countries():
+    info = {}
+    try:
+        #this is the link to the first page
+        url = 'https://safetravel.govt.nz/travel-advisories-destination'
+
+        #set up the headless chrome driver
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        # create a new chrome session
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.implicitly_wait(19)
+        driver.get(url)
+
+        #Selenium hands the page source to Beautiful Soup
+        soup=BeautifulSoup(driver.page_source, 'lxml')
+
+        #patter of the link to the country page that the href should match
+        reg = regex.compile(r'\w+-*')
+        table = soup.find('table')
+        table_body = table.find('tbody')
+        table_rows = table_body.find_all('tr')
+
+        for tr in table_rows:
+            cols = tr.find_all('td')
+            cols = [ele.text.strip() for ele in cols]
+
+            name = cols[1]
+            a = tr.find('a', attrs = {'href':reg})
+            info[name] = {"href":a['href']}
+           
+
+    finally:
+        driver.close()
+        driver.quit()
+
+    return info
+
+
+
+
+#the two functions below should be puth in chrome driver class
+def create_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(19)
+    return driver
+
+def quit_driver(driver):
+    driver.quit()
+
+
+
+
+
+def save_to_new_zealend():
+
+    url = get_url_of_countries() #this function create its own driver -- to change
+    data = {}
+    driver = create_driver()
+
+    print("hello")
+    for country in url:
+        driver.implicitly_wait(5)
+        name = country
+        print(name,"okey")
+        href = url[country].get("href")
+        
+        link = "https://safetravel.govt.nz/{}".format(href,sep='')
+        advisory_text = parse_a_country(link,driver)
+        
+        
+        visa_info = "na"
+        country_iso = "na"
+        data[name] = {'country-iso':country_iso,'name':name,'advisory-text':advisory_text,'visa-info':visa_info}
+        data = find_all_iso(data)
+
+        with open('./advisory-aus.json', 'w') as outfile:
+             json.dump(data, outfile)
+
+    save_into_db(data)
+
+
+
+
+
+def parse_a_country(url,driver):
+    driver.get(url)
+    #Selenium hands the page source to Beautiful Soup
+    soup=BeautifulSoup(driver.page_source, 'lxml')
+    warning = " "
+    for tag in soup.findAll('i'):
+      if tag.parent.name == 'h1':
+          warning = tag.parent.text.strip()
+
+    return warning
+
+
+
+
+def save_into_db(data):
+    # create an an sqlite_advisory object
+    sqlite = sqlite_advisories('newzealand')
+    sqlite.delete_table()
+    sqlite.create_table()
+    for country in data:
+        iso = data[country].get('country-iso')
+        name = data[country].get('name')
+        text = data[country].get('advisory-text')
+        visa_info = data[country].get('visa-info')
+        sqlite.new_row(iso,name,text,visa_info)
+    sqlite.commit()
+    sqlite.close()
+
+
+save_to_new_zealend()
