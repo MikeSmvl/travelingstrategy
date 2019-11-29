3d468d0057b66a5ce15c0a2035cd934adb710080
Charles <chanc09@gmail.com>
2019-11-29 16:17:23 -0500

    #91 added incomplete save method

--- server/data/electrical-sockets.py
+++ server/data/electrical-sockets.py
@@ -8,3 +8,28 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from helper_class.country_names import find_iso_of_country
 
+def get_electrical_sockets():
+    info = {}
+    try:
+        url = 'https://www.worldstandards.eu/electricity/plug-voltage-by-country/'
+        return info
+
+    finally:
+        driver.close()
+        driver.quit()
+
+def save_electrical_sockets():
+    con  = sqlite3.connect('../countries.sqlite')
+    cur = con.cursor()
+
+    cur.execute('DROP TABLE IF EXISTS sockets')
+    con.commit()
+    cur.execute('CREATE TABLE sockets (country_iso VARCHAR, country_name VARCHAR, types VARCHAR)')
+    con.commit()
+
+
+    socket_data = get_electrical_sockets()
+    # something needs to change here
+
+    con.commit()
+    con.close()
