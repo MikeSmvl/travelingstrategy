71e3b5b0b2fa3169aeb8d36204f46ba4649a5508
unknown <tdelaportas@hotmail.com>
2019-11-30 18:32:09 -0500

    #104 #94 Added visa parser for the UK

--- server/data/advisory_uk.py
+++ server/data/advisory_uk.py
@@ -3,6 +3,8 @@ import regex
 from helper_class.chrome_driver import create_driver
 from helper_class.chrome_driver import quit_driver
 from helper_class.sqlite_advisories import sqlite_advisories
+from helper_class.country_names import find_all_iso
+from helper_class.sqlite_advisories import sqlite_advisories
 
 
 # Used to translate using the googletrans library
@@ -37,7 +39,7 @@ def get_url_of_countries():
     return info
 
 
-def parse_one_country_advisory(url):
+def parse_a_country_advisory(url):
     driver = create_driver()
     driver.get(url)
     #Selenium hands the page source to Beautiful Soup
@@ -49,8 +51,88 @@ def parse_one_country_advisory(url):
     return advisory
 
 
-def save_to_db():
-    advisory = parse_one_country_advisory("https://www.gov.uk/foreign-travel-advice/{}/travel-advice-help-and-support".format('afghanistan',sep=''))
+def parse_a_country_visa(url, driver):
+    info ={}
+    driver.get(url)
+    #Selenium hands the page source to Beautiful Soup
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    visa = " "
+    table = soup.find('table')
+    table_body = table.find('tbody')
+    table_rows = table_body.find_all('tr')
+    x = 0
+    for tr in table_rows:
+         x = x+1
+         cols = tr.find_all('td')
+         cols = [ele.text.strip() for ele in cols]
+         name = cols[0]
+         if(x < 5):
+           visaLength = len(cols[1])-3
+           visa = cols[1][0:visaLength]
+         elif( x < 80):
+           visaLength = len(cols[1])-4
+           visa = cols[1][0:visaLength]
+         else:
+           visaLength = len(cols[1])-5
+           visa = cols[1][0:visaLength]
+         if(visa[len(visa)-1: len(visa)] == ']'):
+              if(x < 5):
+                visaLength = len(visa)-3
+                visa = visa[0:visaLength]
+              elif( x < 80):
+                visaLength = len(visa)-4
+                visa = visa[0:visaLength]
+              else:
+                visaLength = len(visa)-5
+                visa = visa[0:visaLength]
+         if(name == "Angola"):
+           visaLength = len(visa)-2
+           visa = visa[0:visaLength]
+
+         info[name] = {"visa":visa}
+
+    return info
+
+
+def save_to_UK():
+    url = get_url_of_countries() #this function create its own driver -- to change
+    data = {}
+    driver = create_driver()
+    visas = parse_a_country_visa("https://en.wikipedia.org/wiki/Visa_requirements_for_British_citizens",driver)
+    counter_country = 0
+
+    for country in url:
+        driver.implicitly_wait(5)
+        name = country
+        href = url[country].get("href")
+
+        link = "https://safetravel.govt.nz/{}".format(href,sep='')
+        advisory_text = parse_a_country_advisory(link)
+
+        visa_text= ""
+        for countryVisa in visas:
+            if(countryVisa ==  country):
+               visa_text = visas[countryVisa].get('visa')
+               break;
+
+        country_iso = "na"
+        data[name] = {'country-iso':country_iso,'name':name,'advisory-text':advisory_text,'visa-info':visa_text}
+        data = find_all_iso(data)
+
+        if ((counter_country%50) == 0):
+            quit_driver(driver)
+            driver = create_driver()
+        counter_country += 1
+
+    with open('./advisory-uk.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    save_into_db(data)
+
+
+
+def save_into_db():
+    advisory = parse_a_country_advisory("https://www.gov.uk/foreign-travel-advice/{}/travel-advice-help-and-support".format('afghanistan',sep=''))
 
     # create an an sqlite_advisory object
     sqlite = sqlite_advisories('GB')
@@ -71,4 +153,4 @@ def save_to_db():
 
 
 if __name__ == '__main__':
-    save_to_db()
+    save_into_db()
