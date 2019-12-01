38ad3ee3ac0fb1b2cfc40758cf1ec4e0da24aa14
Mikael Samvelian <mikael.samvelian@gmail.com>
2019-12-01 01:37:14 -0500

    #109 Linting fixes

--- client/src/pages/Country.js
+++ client/src/pages/Country.js
@@ -44,7 +44,7 @@ function Country({ origin, destination }) {
 
 	useEffect(() => {
 		async function fetchData() {
-			setIsLoading(true)
+			setIsLoading(true);
 			await fetch('http://localhost:4000/', {
 				method: 'POST',
 				headers: { 'Content-Type': 'application/json' },
@@ -74,16 +74,16 @@ function Country({ origin, destination }) {
 						setAdvisory('Not available yet.');
 						setVisa('Not available yet.');
 						setLanguages('Not available yet.');
-						setIsLoading(false)
+						setIsLoading(false);
 					} else {
 						setAdvisory(res.data.countryToCountry[0].advisory_text);
 						setVisa(res.data.countryToCountry[0].visa_info);
 						setLanguages(res.data.country_languages[0]);
-						setIsLoading(false)
+						setIsLoading(false);
 					}
 				});
 		}
-		fetchData()
+		fetchData();
 	}, [origin, destination]);
 
 	const countryCode = getCountryCode(destination);
@@ -94,70 +94,71 @@ function Country({ origin, destination }) {
 	}
 	return (
 		<div>
-			{!isLoading && <ReactFullpage
-				licenseKey="CF1896AE-3B194629-99B627C1-841383E5"
-				scrollingSpeed={1000} /* Options here */
-				sectionsColor={['rgb(232, 233, 241)', 'rgb(255, 222, 206)']}
-				navigation
-				navigationPosition="left"
-				navigationTooltips={['Basics', 'Health & Safety', 'Money']}
-				anchors={['basics', 'health', 'money']}
-				scrollOverflow
-				normalScrollElements=".scrolling-card"
-				responsiveWidth={800}
-        afterResponsive={function(isResponsive){}}
-				render={({ state, fullpageApi }) => {
-					return (
-						<ReactFullpage.Wrapper>
-							<div className="section App">
-								<Header title={getCountryName(destination)} />
-								<Subtitle text="Important Basics" />
-								<Row
-									className="justify-content-center"
-									style={{ padding: '5px 25px' }}
-								>
-									<Col xs="10" sm="4">
-										<CountryCard flagSrc={src} title="Country Flag">
-											<CardBody>
-												{Languages(languagesInfo)}
-												{languagesInfo === 'Not available yet.' && (
-													<div style={{ paddingBottom: '5px' }}>
+			{!isLoading && (
+				<ReactFullpage
+					licenseKey="CF1896AE-3B194629-99B627C1-841383E5"
+					scrollingSpeed={1000} /* Options here */
+					sectionsColor={['rgb(232, 233, 241)', 'rgb(255, 222, 206)']}
+					navigation
+					navigationPosition="left"
+					navigationTooltips={['Basics', 'Health & Safety', 'Money']}
+					anchors={['basics', 'health', 'money']}
+					scrollOverflow
+					normalScrollElements=".scrolling-card"
+					responsiveWidth={800}
+					render={({ state, fullpageApi }) => {
+						return (
+							<ReactFullpage.Wrapper>
+								<div className="section App">
+									<Header title={getCountryName(destination)} />
+									<Subtitle text="Important Basics" />
+									<Row
+										className="justify-content-center"
+										style={{ padding: '5px 25px' }}
+									>
+										<Col xs="10" sm="4">
+											<CountryCard flagSrc={src} title="Country Flag">
+												<CardBody>
+													{Languages(languagesInfo)}
+													{languagesInfo === 'Not available yet.' && (
+														<div style={{ paddingBottom: '5px' }}>
 														Not Available yet.
-													</div>
-												)}
-											</CardBody>
-										</CountryCard>
-									</Col>
-									<Col xs="10" sm="4">
-										<Card className="scrolling-card" header="Visa Info" style={{height: '400px', overflow: 'scroll'}}>
-											<CardBody className="scrolling-card" style={{ paddingTop: '0' }}>
-												<div className="scrolling-card" dangerouslySetInnerHTML={{ __html: visaInfo}} />
-											</CardBody>
-										</Card>
-									</Col>
-									<Col xs="10" sm="4">
-										<Card header="Advisory">
-											<CardBody>
-												<ErrorOutlineOutlinedIcon style={{ color: '#dc3545' }} />
-												{' '}
-												{JSON.stringify(advisoryInfo).replace(/(^")|("$)/g, '')}
-											</CardBody>
-										</Card>
-									</Col>
-								</Row>
-							</div>
+														</div>
+													)}
+												</CardBody>
+											</CountryCard>
+										</Col>
+										<Col xs="10" sm="4">
+											<Card className="scrolling-card" header="Visa Info" style={{ height: '400px', overflow: 'scroll' }}>
+												<CardBody className="scrolling-card" style={{ paddingTop: '0' }}>
+													<div className="scrolling-card" dangerouslySetInnerHTML={{ __html: visaInfo }} />
+												</CardBody>
+											</Card>
+										</Col>
+										<Col xs="10" sm="4">
+											<Card header="Advisory">
+												<CardBody>
+													<ErrorOutlineOutlinedIcon style={{ color: '#dc3545' }} />
+													{' '}
+													{JSON.stringify(advisoryInfo).replace(/(^")|("$)/g, '')}
+												</CardBody>
+											</Card>
+										</Col>
+									</Row>
+								</div>
 
-							<div className="section">
-								<Subtitle text="Health & Safety" />
-							</div>
+								<div className="section">
+									<Subtitle text="Health & Safety" />
+								</div>
 
-							<div className="section">
-								<Subtitle text="Money" />
-							</div>
-						</ReactFullpage.Wrapper>
-					);
-				}}
-			/>}
+								<div className="section">
+									<Subtitle text="Money" />
+								</div>
+							</ReactFullpage.Wrapper>
+						);
+					}}
+				/>
+			)}
 		</div>
 	);
 }
