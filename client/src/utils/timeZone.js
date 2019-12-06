6d339c58f826a09aa7155f3af53fd187f245fcc4
Oumar96 <oumarba221296@hotmail.fr>
2019-12-06 14:59:26 -0500

    #92 Time difference between country of origin and destination

--- client/src/utils/timeZone.js
+++ client/src/utils/timeZone.js
@@ -1,7 +1,58 @@
 const cityTimezones = require('city-timezones');
 var country = require('country-list-js');
+var originNotFound = false;
+var destinationNotFound = false;
+var capitalIfOriginNotFound = "";
+var capitalIfDestinationNotFound = "";
 
-function getTimeZone(city,destinationCountry){
+/**
+ * This method finds the difference between two dates
+ * and returns the time at the destination and
+ * the time difference between the origin and destination
+ */
+function findTimeZoneDifference(originCity,destinationCity,originCountry,destinationCountry){
+    var timeZoneAndDifference
+    var timeZoneAtOrgin= getTimeZone(originCity, originCountry, "origin")
+    var timeZoneAtdestination= getTimeZone(destinationCity, destinationCountry, "destination")
+    timeZoneAtOrgin = switchDayAndMonth(timeZoneAtOrgin)
+    timeZoneAtdestination = switchDayAndMonth(timeZoneAtdestination)
+
+    if(timeZoneAtOrgin == null || timeZoneAtdestination == null){
+        console.log("TimeZone for origin or destination cannot be found")
+        return null
+    }
+
+    const dateAtOrigin = new Date(timeZoneAtOrgin);
+    const dateAtDestination = new Date(timeZoneAtdestination);
+    var timeDifference = Math.round((dateAtDestination - dateAtOrigin)/3600000) //Converting to hours
+    if(originNotFound){
+        originCity = capitalIfOriginNotFound
+    }
+    if(destinationNotFound){
+        timeZoneAtdestination = "Time in the capital the country of destination "+ capitalIfDestinationNotFound + " "+timeZoneAtdestination
+    }
+
+    console.log("Time at origin:"+dateAtOrigin)
+    console.log("Time at destination:"+dateAtDestination)
+    console.log("Time difference: "+timeDifference)
+
+    if(timeDifference>0){ //Showing time difference with +
+        timeZoneAndDifference = timeZoneAtdestination + " (+"+timeDifference+"H from "+originCity+")"
+    }
+    else{ //Showing time difference with -
+        timeZoneAndDifference = timeZoneAtdestination + " ("+timeDifference+"H from "+originCity+")"
+    }
+    return timeZoneAndDifference
+}
+
+/**
+ * The function returns the time at a given city
+ * @param {*} city This is the city for which we need the time
+ * @param {*} destinationCountry  If we don't find the time of city we get the time in the capital
+ * @param {*} originOrDestination To mention if the city is the origin or destination
+ */
+function getTimeZone(city,destinationCountry, originOrDestination){
+    console.log("Getting time in:"+city)
     var timeAtLocation
 
     try{
@@ -16,44 +67,62 @@ function getTimeZone(city,destinationCountry){
 
         timeAtLocation = (new Date()).toLocaleString([], options)
         timeAtLocation = timeAtLocation.substr(0,timeAtLocation.length-3) //Removing seconds
-        // console.log("city: "+destinationCity)
-        // console.log("time-zone: "+timezone)
-        // console.log("time At Location: "+timeAtLocation)
+        console.log("city: "+city)
+        console.log("time-zone: "+timezone)
+        console.log("time At Location: "+timeAtLocation)
     }
     catch(error){
-        // console.log("Could not get timezone of "+city)
+        console.log("Could not get timezone of "+city)
         var firstE = city.indexOf("e")
         city = city.substr(0,firstE) + "é" + city.substr(firstE+1) // replacing the first e with é
-        timeAtLocation = handleErrorForSomeCities(city,destinationCountry, firstE)
+        timeAtLocation = handleErrorForSomeCities(city,destinationCountry, firstE,originOrDestination)
         return timeAtLocation
     }
     return timeAtLocation
 
 }
 
-function handleErrorForSomeCities(city,destinationCountry, firstE){
+/**
+ * If the city chosen to find the time cannot be found in the library
+ * We find the timezone in the capital of the country
+ */
+function handleErrorForSomeCities(city,destinationCountry, firstE,originOrDestination){
     var timeZone
     if(firstE >= 0){
         city = city.substr(0,firstE) + "é" + city.substr(firstE+1) // replacing the first e with é
-        timeZone = getTimeZone(city,destinationCountry)
+        timeZone = getTimeZone(city,destinationCountry, originOrDestination)
         return timeZone
     }
     else{ // Return the time zone of the capital of the country of destination
-        // console.log("Getting time zone of the capital of the country of destination("+destinationCountry+")")
-        var countryOfDestination = country.findByIso2(destinationCountry)
-        timeZone = getTimeZone(countryOfDestination.capital,destinationCountry)
-        var timeZoneOfCapital = "Time in the capital "+ countryOfDestination.capital+ " "+timeZone
-        // console.log(timeZoneOfCapital)
-        return(timeZoneOfCapital)
+        console.log("Getting time zone of the capital of the country of destination("+destinationCountry+")")
+        var countryForCapital = country.findByIso2(destinationCountry)
+
+        timeZone = getTimeZone(countryForCapital.capital,destinationCountry)
+
+        if(originOrDestination === "origin"){
+            originNotFound = true
+            capitalIfOriginNotFound = countryForCapital.capital
+        }
+        else if(originOrDestination === "destination"){
+            destinationNotFound = true
+            capitalIfDestinationNotFound = countryForCapital.capital
+        }
+        return(timeZone)
     }
 }
 
-function findTimeZoneDifference(originCity,destinationCity,originCountry,destinationCountry){
-    // var timeZoneAtOrgin= getTimeZone(originCity, originCountry)
-    var timeZoneAtdestination= getTimeZone(destinationCity, destinationCountry)
-    // var timeZoneAndDifference = timeZoneAtOrgin + "\n\n\n" + timeZoneAtdestination
-    // console.log(timeZoneAndDifference)
-    return timeZoneAtdestination
+/**
+ *  The date function takes the following format: day/month/year
+ *  However the timezone we have is in the format: month/day/year
+ *  This function switches the day and the mont
+ *  */
+function switchDayAndMonth(time){
+    var timeSplit = time.split("/")
+    var dayMonthSwitched = timeSplit[1]+"/"+timeSplit[0]+"/"+timeSplit[2]
+
+    return dayMonthSwitched
 }
+
+
 
 export default findTimeZoneDifference;
