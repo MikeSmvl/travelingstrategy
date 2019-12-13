e7e7c3ce71fb61b184765052ead488cc4a3dbbef
Oumar96 <oumarba221296@hotmail.fr>
2019-12-13 17:12:47 -0500

    #138 adding electrical and money back in db

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,7 @@ function Application() {
 					<Route exact path="/" component={Home} />
 				</Route>
 				<Route path="/country">
-					<Country originCountry={query.get('originCountry')} destinationCountry={query.get('destinationCountry')} 
+					<Country originCountry={query.get('originCountry')} destinationCountry={query.get('destinationCountry')}
 					originCity={query.get('originCity')} destinationCity={query.get('destinationCity')}
 					originLat={query.get('originLat')} originLng={query.get('originLng')}
 					destinationLat={query.get('destinationLat')} destinationLng={query.get('destinationLng')}/>
