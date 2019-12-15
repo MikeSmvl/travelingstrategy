542c89d908509bea1149ae64cd2c3b2ce8be1321
unknown <tdelaportas@hotmail.com>
2019-12-15 17:01:40 -0500

    #126 Pulled from master and made some changes

--- server/data/advisory_us.py
+++ server/data/advisory_us.py
@@ -7,7 +7,7 @@ import regex
 from helper_class.chrome_driver import create_driver, quit_driver
 from helper_class.country_names import find_all_iso
 from helper_class.sqlite_advisories import sqlite_advisories
-from helper_class.visa_info import parse_a_country_visa
+from helper_class.wiki_visa_parser import wiki_visa_parser
 
 def get_name_and_advisory_of_countries():
     try:
@@ -52,13 +52,15 @@ def save_to_united_states():
     name_advisory = get_name_and_advisory_of_countries()
     info ={}
 
-    visas = parse_a_country_visa("https://en.wikipedia.org/wiki/Visa_requirements_for_United_States_citizens")
+    driver = create_driver()
+    wiki_visa_url = "https://en.wikipedia.org/wiki/Visa_requirements_for_United_States_citizens"
+    wiki_visa_ob = wiki_visa_parser(wiki_visa_url,driver)
+    visas = wiki_visa_ob.visa_parser_table()
 
     for name in sorted (name_advisory.keys()):
        info[name] = name_advisory[name]
 
     data = {}
-    driver = create_driver()
     counter_country = 0
 
     for country in info:
