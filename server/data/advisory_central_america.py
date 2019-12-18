b0ec8f67d30b3e03ca909c9d067f97f767fc264d
LineG <ghanemline@gmail.com>
2019-12-18 14:37:51 -0500

    #116 added names in spanish vs iso

--- server/data/advisory_central_america.py
+++ server/data/advisory_central_america.py
@@ -14,6 +14,7 @@ from helper_class.flags import Flags
 from helper_class.logger import Logger
 from lib.config import currency_api_link, iso_list, sqlite_db, wiki_visa_url_BZ
 from lib.database import Database
+from helper_class.en_vs_es_country_names import get_iso_es
 
 # This file will encompass the parsing of 5 countries:
 # Belize, Dominica, Dominican republic, Mexico & Panama.
@@ -32,17 +33,16 @@ DB = Database(sqlite_db)
 
 def save_into_db(tableName, data):
     # create an an sqlite_advisory object
-    sqlite = sqlite_advisories(tableName)
-    sqlite.delete_table()
-    sqlite.create_table()
+    DB.add_table(tableName,country_iso="country_iso"
+        ,name="name",advisory_text="advisory_text",visa_info="visa_info")
     for country in data:
         iso = data[country].get('country-iso')
         name = data[country].get('name')
         text = data[country].get('advisory-text')
         visa_info = data[country].get('visa-info')
-        sqlite.new_row(iso,name,text,visa_info)
-    sqlite.commit()
-    sqlite.close()
+        DB.insert(tableNAme,country_iso=iso,name=name,
+            advisory_text=text,visa_info=visa_info)
+
 
 def mexico_all_links(driver):
     url = 'https://guiadelviajero.sre.gob.mx/'
@@ -51,9 +51,29 @@ def mexico_all_links(driver):
     reg = regex.compile(r'\/103-ficha-de-paises\/')
     a = soup.findAll('a', attrs = {'href':reg})
     links = {}
+    iso_es = get_iso_es()
+    c = 0
     for att in a:
-        links[att.text] = 'https://guiadelviajero.sre.gob.mx/'+att['href']
+        try:
+            c += 1
+            name = att.text.strip()
+            iso = iso_es[name]
+            href= {'https://guiadelviajero.sre.gob.mx'+att['href']}
+            links[name] = {'advisory_text':href,'country_iso':iso,'name':name}
+        except:
+            print("This country's iso was not found:",att.text)
+    #print(links,c)
     return links
+
+#function to replace name by iso
+def replace_key_by_iso(data):
+    data = find_all_iso(data)
+    data_new = {}
+    for k in data:
+        iso = data[k].get('country-iso')
+        visa_info = data[k].get('visa')
+        data_new[iso] = {'name': k, 'visa-info':visa_info}
+    return data_new
 
 def save_to_central_america():
 
@@ -65,6 +85,8 @@ def save_to_central_america():
     dataPA = {}
     driver = create_driver()
     mexico_advidory = mexico_all_links(driver)
+    # mexico_advidory = replace_key_by_iso(mexico_advidory)
+    # print(mexico_advidory)
     # wiki_visa_ob_BZ = wiki_visa_parser(wiki_visa_url_BZ, driver)
     # wiki_visa_ob_DM = wiki_visa_parser(wiki_visa_url_DM, driver)
     # wiki_visa_ob_DO = wiki_visa_parser(wiki_visa_url_DO, driver)
