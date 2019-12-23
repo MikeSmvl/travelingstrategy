8da3c12a54a065c2bde4e9b64ee6cb83e99dced9
Oumar96 <oumarba221296@hotmail.fr>
2019-12-23 10:24:18 -0500

    #118 #151 Getting url for countries given the starting letter

--- server/data/advisory_sg.py
+++ server/data/advisory_sg.py
@@ -1,9 +1,39 @@
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
+import string
 # import regex
 from helper_class.chrome_driver import create_driver, quit_driver
 # from helper_class.sqlite_advisories import sqlite_advisories
 from helper_class.country_names import find_iso_of_country, find_all_iso
 from helper_class.wiki_visa_parser import wiki_visa_parser
+
+def get_url_of_for_letter(dictionnary, letter):
+    try:
+        #this is the link to the first page
+        url = 'https://www.mfa.gov.sg/Where-Are-You-Travelling-To?letter={}'.format(letter,sep='')
+
+        driver = create_driver()
+        driver.get(url)
+
+        #Selenium hands the page source to Beautiful Soup
+        soup=BeautifulSoup(driver.page_source, 'lxml')
+        #All countries for a given letter
+        countries = soup.findAll("a", {"class": "embassy-dropdown"})
+
+        # #retrieving links for all countries of the alphabet
+        for country in countries:
+            country_name = country.text
+            country_iso = find_iso_of_country(country_name)
+            if(country_iso != ""): #Countries that don't have iso are not official counntries
+                href = country['href']
+                dictionnary[country_iso] = {"href":href}
+    finally:
+        driver.close()
+        driver.quit()
+    print(dictionnary)
+
+    return dictionnary
+
+
 
 def save_to_SG():
     driver = create_driver()
@@ -26,4 +56,6 @@ def save_to_SG():
 
     quit_driver(driver)
 if __name__ == '__main__':
-    save_to_SG()
+    info = {}
+    get_url_of_for_letter(info, 'a')
+    # save_to_SG()
