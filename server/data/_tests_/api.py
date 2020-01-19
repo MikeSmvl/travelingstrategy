aafe372d0498a56e3715c461b505ca1939a0d143
Mikael Samvelian <mikael.samvelian@gmail.com>
2020-01-18 20:20:37 -0500

    #179 Add unit test for united nations data api

--- server/data/_tests_/api.py
+++ server/data/_tests_/api.py
@@ -8,7 +8,8 @@ data_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 sys.path.append(data_dir)
 from helper_class.api_helper import ApiHelper
-from lib.config import currency_api_link
+from helper_class.url_helper import UrlHelper
+from lib.config import currency_api_link, united_nations_api_link
 
 class TestApiHelper(unittest.TestCase):
 
@@ -19,15 +20,37 @@ class TestApiHelper(unittest.TestCase):
         with self.assertRaises(Exception):
             ApiHelper('https://')
 
-    def test_working_api(self):
+    def test_currency_api(self):
         """
-        Test that uses one of our API links to make sure it returns a valid HTML code.
-        The expected response code is 400 as a specific country is not given.
+        Test that uses one of our API links (currency) to make sure it returns a valid HTML code.
+        The expected response code is 200 as the endpoint should be up.
         """
         try:
-            api = ApiHelper(currency_api_link)
+            country = 'CA' # Test specific country (Canada)
+            url_converter = UrlHelper(currency_api_link)
+            information_link = url_converter.get_currency_api(country)
+
+            api_no_country = ApiHelper(currency_api_link)
+            api_with_country = ApiHelper(information_link)
+
+            error_code = api_no_country.get_code()
+            self.assertEqual(str(error_code), '<Response [400]>')
+
+            success_code = api_with_country.get_code()
+            self.assertEqual(str(success_code), '<Response [200]>')
+        except Exception:
+            self.fail("Api link raised an exception!")
+
+    def test_un_api(self):
+        """
+        Test that uses one of our API links (united nations) to make sure it returns a valid HTML code.
+        The expected response code is 200 as the endpoint should be up.
+        """
+        try:
+            api = ApiHelper(united_nations_api_link)
             code = api.get_code()
-            self.assertEqual(str(code), '<Response [400]>')
+
+            self.assertEqual(str(code), '<Response [200]>')
         except Exception:
             self.fail("Api link raised an exception!")
 
