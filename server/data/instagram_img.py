b90a3933a9661154e6d7424f3db33f159df09757
Charles <chanc09@gmail.com>
2020-02-05 15:34:52 -0500

    #195 added functions for caption and geolocation, move static url to config file

--- server/data/instagram_img.py
+++ server/data/instagram_img.py
@@ -13,24 +13,64 @@ from helper_class.wiki_visa_parser import wiki_visa_parser
 from helper_class.flags import Flags
 from helper_class.logger import Logger
 from lib.database import Database
+from lib.config import instagram_url
 
+# so were gonna want to get/do
+# 1. the caption C4VMK
+# 2. the geolocation, especially if its a restaurant O4GlU
+# 3. we need to save, we need to talk about how to make the tablees
+# 4. we need to gather images on a time scale (2-7 days for now)
 
-def find_images(tag):
+def find_images(location):
     driver = create_driver()
-    url = "https://www.instagram.com/explore/tags/"+tag+"/"
+    url = instagram_url + location + "/"
     driver.get(url)
-    soup=BeautifulSoup(driver.page_source, 'lxml')
+    soup = BeautifulSoup(driver.page_source, 'lxml')
     img = soup.find_all('img', {'class':'FFVAD'})
     count = 0
     for i in img:
         count += 1
-        #what instagram recognition think is in the photo
+        # what instagram recognition think is in the photo
         print(count)
         print(i.get('alt'))
-        #link to the photo
+        # link to the photo
         print(i.get('src'),"\n")
-
 
     quit_driver(driver)
 
-find_images("montreal")
+def find_caption(location):
+    driver = create_driver()
+    url = instagram_url + location + "/"
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    caption = soup.find_all('div', {'class':'C4VMK'})
+    count = 0
+    print("test")
+    for c in caption:
+        count += 1
+        print(count)
+        print(c.get('title'))
+
+    quit_driver(driver)
+
+
+def find_geolocation(location):
+    driver = create_driver()
+    url = instagram_url + location + "/"
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    geoloc = soup.find_all('a', {'class':'O4GlU'})
+    count = 0
+    print("test")
+    for g in geoloc:
+        count += 1
+        print(count)
+        print(g.get('href'))
+
+    quit_driver(driver)
+
+# saving function
+
+#find_images("newyork")
+#find_caption("newyork")
+find_geolocation("newyork")
