393356999952c2afc9381aaae39d13f12313a0f4
LineG <ghanemline@gmail.com>
2020-02-09 15:42:25 -0500

    refactoring the image parser #195

--- server/data/instagram_img.py
+++ server/data/instagram_img.py
@@ -21,64 +21,62 @@ from lib.config import instagram_url
 # 3. we need to save, we need to talk about how to make the tablees
 # 4. we need to gather images on a time scale (2-7 days for now)
 
-def find_images(location):
+# Initialize flags, logger & database
+FLAGS = Flags()
+LEVEL = FLAGS.get_logger_level()
+LOGGER = Logger(level=LEVEL) if LEVEL is not None else Logger()
+DB = Database("countries.sqlite")
+
+def find_a_post(location):
     driver = create_driver()
+
     url = instagram_url + location + "/"
     driver.get(url)
     soup = BeautifulSoup(driver.page_source, 'lxml')
-    img = soup.find_all('img', {'class':'FFVAD'})
+    garb_all = soup.find_all('a', {'href':regex.compile(r'/p/')})
+
     count = 0
-    for i in img:
+
+    for g in garb_all:
         count += 1
-        # what instagram recognition think is in the photo
-        print(count)
-        print(i.get('alt'))
-        # link to the photo
-        print(i.get('src'),"\n")
-
-    quit_driver(driver)
-
-def find_caption(location):
-    driver = create_driver()
-    url = instagram_url + location + "/"
-    driver.get(url)
-    soup = BeautifulSoup(driver.page_source, 'lxml')
-    caption = soup.find_all('div', {'class':'C4VMK'})
-    count = 0
-    print("test")
-    for c in caption:
-        count += 1
-        print(count)
-        print(c.get('title'))
-
-    quit_driver(driver)
-
-
-def find_geolocation(location):
-    driver = create_driver()
-    url = instagram_url + location + "/"
-    driver.get(url)
-    soup = BeautifulSoup(driver.page_source, 'lxml')
-    geoloc = soup.find_all('a', {'href':regex.compile(r'/p/')})
-    count = 0
-    print("test")
-    for g in geoloc:
-        count += 1
+        if count > 10:
+            break
         print(count)
         print(g.get('href'))
         count += 1
         u = "https://www.instagram.com"+g.get('href')
         print(u)
         driver.get(u)
+
         soup = BeautifulSoup(driver.page_source, 'lxml')
         geoloc = soup.find('a',{'class':'O4GlU'})
+        caption = soup.find('div', {'class':'C4VMK'})
+        img = soup.find('img', {'class':'FFVAD'})
+
+        image_info = {}
+
         if not geoloc == None:
             print(geoloc.text)
+            image_info['geolocation'] = geoloc.text
+        if not caption == None:
+            print(caption.text)
+            image_info['caption'] = caption.text
+        if not img == None:
+            print(img.get('alt'))
+            image_info['image_link'] = img.get('src')
+
+        save_to_db("images", image_info)
 
     quit_driver(driver)
 
 # saving function
+def save_to_db(tableName,image_info):
+    image_link = image_info['caption']
+    geolocation = image_info['geolocation']
+    caption = image_info['caption']
+    DB.drop(tableName)
+    DB.add_table(tableName,image_id="image_id INTEGER PRIMARY KEY", image_link="image_link",
+            geolocation="geolocation", cation="caption")
+    DB.insert(tableName, "null",image_link, geolocation, caption)
 
-#find_images("newyork")
-#find_caption("newyork")
-find_geolocation("newyork")
+find_a_post("newyork")
