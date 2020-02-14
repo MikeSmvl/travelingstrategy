9bd3d2c7256d021aef4eb684bac460af1163d485
unknown <tdelaportas@hotmail.com>
2020-02-14 03:54:04 -0500

    #215 #230 Fixed sql statement to return image_links

--- server/data/sendEmail.py
+++ server/data/sendEmail.py
@@ -11,6 +11,17 @@ LOGGER = Logger(level=LEVEL) if LEVEL is not None else Logger()
 DB = Database("countries.sqlite")
 
 
+
+def getImages():
+  
+  query = "SELECT image_link FROM images WHERE tag='newyork'"
+  db = Database("countries.sqlite")
+  x = db.query(query)
+  for image in x:
+    print (image)
+
+getImages()
+    
 email_html = Email_html()
 email_html.add_image("https://content.fortune.com/wp-content/uploads/2019/09/Intrepid-Travel-sri-lanka_dambulla_sigiriya-lion-rock-fortress_group.jpg","246", "246", "Sri Lanka")
 email_html.add_image("https://content.fortune.com/wp-content/uploads/2019/09/1920x1080-Intrepid-Travel-Egypt-Cairo-Pyramids-group-hug-028.jpeg", "246", "185", "Egypt")
@@ -18,17 +29,9 @@ email_html.add_image("https://appboy-images.com/appboy/communication/assets/imag
 
 html = email_html.get_email()
 
-def getImages():
-  
-  query = "SELECT tag FROM images"
-  db = Database("countries.sqlite")
-  x = db.query(query)
-  for image in x:
-    print (image)
 
 
 
-getImages()
 LOGGER.info(f'Getting information from table subscribers.')
 subscribers = DB.get_items("subscribers")
 for user in subscribers:
