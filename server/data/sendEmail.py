dece0b8485e1edaeea632b1a3a14a10ac96daee7
steffan <steffan_v@hotmail.com>
2020-02-16 14:57:18 -0500

    #215 added loggers for sending email

--- server/data/sendEmail.py
+++ server/data/sendEmail.py
@@ -13,37 +13,51 @@ DB = Database("countries.sqlite")
 
 
 def getImages(city):
-  stringCity = "'"+city+"'"
-  query = "SELECT image_link FROM images WHERE tag="+stringCity
-  x = DB.query(query)
-  email_html = Email_html()
-  count = 0
+  try:
+    LOGGER.info(f'Retrieving pictures for: {stringCity}')
+    stringCity = "'"+city+"'"
+    query = "SELECT image_link FROM images WHERE tag="+stringCity
+    x = DB.query(query)
+    email_html = Email_html()
+    count = 0
 
-  for image in x:
-    if(count <= 5):
-      x = ' '.join(image)
-      if(count == 0):
-        email_html.add_left_image(x,"246", "246", "Sri Lanka")
-      elif(count == 1):
-        email_html.add_right_image(x, "246", "139", "Peru")
-      elif(count == 2):
-        email_html.add_left_image(x,"246", "185", "Sri Lanka")
-      elif(count == 3):
-        email_html.add_right_image(x, "246", "246", "Peru")
-      elif(count == 4):
-        email_html.add_left_image(x,"246", "139", "Sri Lanka")
-      else:
-        email_html.add_right_image(x, "246", "185", "Peru")
-      count  = count +1
-  return  email_html.get_email()
+    for image in x:
+      if(count <= 5):
+        x = ' '.join(image)
+        if(count == 0):
+          email_html.add_left_image(x,"246", "246", "Sri Lanka")
+          LOGGER.success(f'Picture was succesfully retrieve: {x}')
+        elif(count == 1):
+          email_html.add_right_image(x, "246", "139", "Peru")
+          LOGGER.success(f'Picture was succesfully retrieve: {x}')
+        elif(count == 2):
+          email_html.add_left_image(x,"246", "185", "Sri Lanka")
+          LOGGER.success(f'Picture was succesfully retrieve: {x}')
+        elif(count == 3):
+          email_html.add_right_image(x, "246", "246", "Peru")
+          LOGGER.success(f'Picture was succesfully retrieve: {x}')
+        elif(count == 4):
+          email_html.add_left_image(x,"246", "139", "Sri Lanka")
+          LOGGER.success(f'Picture was succesfully retrieve: {x}')
+        else:
+          email_html.add_right_image(x, "246", "185", "Peru")
+          LOGGER.success(f'Picture was succesfully retrieve: {x}')
+        count  = count +1
+    return  email_html.get_email()
+ 
+  except Exception as error_msg:
+      LOGGER.error(f'The following error has occured while retrieving the image for {stringCity}: {error_msg}')
 
 
 
 LOGGER.info(f'Getting information from table subscribers.')
 subscribers = DB.get_items("subscribers")
 for user in subscribers:
-  LOGGER.info(f'Sending email to {user}.')
-  recipient = user[0]
-  html =getImages(user[1].replace(" ", "").lower()) 
-  email = Email(subject, sender, recipient, html)
-  email.sendEmail(password)
+  try:
+    LOGGER.info(f'Sending email to {user}.')
+    recipient = user[0]
+    html =getImages(user[1].replace(" ", "").lower()) 
+    email = Email(subject, sender, recipient, html)
+    email.sendEmail(password)
+  except Exception as error_msg:
+    LOGGER.error(f'Email could not be sent to {user} for the following reason: {error_msg}')
