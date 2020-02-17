14537e100d609f1d3317f2757893d4ee6c081ec2
unknown <tdelaportas@hotmail.com>
2020-02-16 19:33:47 -0500

    #215 #230 Fixed image sizes

--- server/data/sendEmail.py
+++ server/data/sendEmail.py
@@ -18,14 +18,23 @@ def getImages(city):
   x = DB.query(query)
   email_html = Email_html()
   count = 0
+
   for image in x:
     if(count <= 5):
-      count  = count +1
       x = ' '.join(image)
-      if(count%2 == 0):
+      if(count == 0):
         email_html.add_left_image(x,"246", "246", "Sri Lanka")
-      else:
+      elif(count == 1):
         email_html.add_right_image(x, "246", "139", "Peru")
+      elif(count == 2):
+        email_html.add_left_image(x,"246", "185", "Sri Lanka")
+      elif(count == 3):
+        email_html.add_right_image(x, "246", "246", "Peru")
+      elif(count == 4):
+        email_html.add_left_image(x,"246", "139", "Sri Lanka")
+      else:
+        email_html.add_right_image(x, "246", "185", "Peru")
+      count  = count +1
   return  email_html.get_email()
 
 
@@ -37,5 +46,4 @@ for user in subscribers:
   recipient = user[0]
   html =getImages(user[1].replace(" ", "").lower()) 
   email = Email(subject, sender, recipient, html)
-  print("askndalkdnsasdlsldfhsdklfnslkdfnlskdnfsakfnlkdsl")
   email.sendEmail(password)
