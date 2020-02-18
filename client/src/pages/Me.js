0a7acf3e63955effeab5ea396d401e0211ce1561
Oumar96 <oumarba221296@hotmail.fr>
2020-02-18 17:08:24 -0500

    #196 #246 Setting up basis profile page

--- client/src/pages/Me.js
+++ client/src/pages/Me.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
+import { Row, Col, Table, Nav } from 'react-bootstrap/';
+import { Card, CardBody} from '../components/Card/Card';
+
+import "../App.css";
 
 function Me() {
 	const [redirect, setRedirect] = useState(false);
@@ -13,6 +17,21 @@ function Me() {
 			}
 		}
 
+		async function fetchData() {
+			await fetch('http://localhost:4000/graphql', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					query: `{
+					}`
+				})
+			})
+			.then((res) => res.json())
+			.then((res) => {
+			});
+		}
+
+		fetchData();
 		getToken();
 	});
 
@@ -21,7 +40,43 @@ function Me() {
 	}
 
 	return (
-		<div />
+		<div>
+			<div className="parallax">
+				<Row className="justify-content-center" style={{paddingTop: '300px'}}>
+					<Row className="justify-content-center">
+						<Col
+							style={{
+								backgroundColor: 'rgb(255, 255, 255)',
+								borderRadius: '20px'
+							}}
+							lg={8}
+						>
+							<Row
+									style={{
+										backgroundColor: 'rgb(247,	247,	247)',
+										padding: '0.5em',
+										borderRadius: '0px'
+									}}
+									className="justify-content-center sticky"
+								>
+								<Card
+									style={{
+										width: '395px',
+										height: '255px'
+									}}
+								>
+									<CardBody
+										classExtra="chosen-cities">
+										New York
+									</CardBody>
+								</Card>
+							</Row>
+						</Col>
+					</Row>
+				</Row>
+				<footer id="footer" />
+			</div>
+		</div>
 	);
 }
 
