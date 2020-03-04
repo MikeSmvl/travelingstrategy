1c503c48d8a1d5c2e46682123a49095b786e98d3
steffan <steffan_v@hotmail.com>
2020-03-04 14:34:22 -0500

    #268 added functionality to the buttons

--- client/src/components/Slider/Slider.js
+++ client/src/components/Slider/Slider.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Slider.css';
 
 function Slider() {
   //array to show the components for the Slider
   let slideArray =  [1,2,3]
+  const [x, setX] = useState(0)
+  //functionality for the buttons
+  const goLeft=()=> {
+    //if x is equal to the first component, clicking left will set set the slider back to the last component
+    //otherwise keep sliding to the left
+    x === 0 ? setX(-100 * (slideArray.length -1)) : setX(x + 100)
+  }
+
+  const goRight=()=> {
+    //if x is equal to the length of the array, clicking right will set the slider back to the first component
+    //otherwise keep sliding to the right
+    x === -100 * (slideArray.length -1) ? setX(0) : setX(x - 100)
+  }
+
   return(
     <div className="slider">
       {
         slideArray.map((item, index)=>{
           return(
-            <div key={index} className="slide">
+            <div key={index} className="slide" style={{transform:`translateX(${x}%)`}}>
               {item}
             </div>
           )
@@ -17,8 +31,8 @@ function Slider() {
       }
 
     {/* button to navigate left or right */}
-    <button id="leftButton">left</button>
-    <button id="rightButton">right</button>
+    <button id="leftButton" onClick={goLeft}>left</button>
+    <button id="rightButton" onClick={goRight}>right</button>
     </div>
   )
 }
