023cea603bc4b1f3dc320533dc4f4938c63c941a
steffan <steffan_v@hotmail.com>
2020-03-05 01:10:46 -0500

    #268 added new page for user selection for vacation spot

--- client/src/pages/TrendingSpots.js
+++ client/src/pages/TrendingSpots.js
@@ -3,16 +3,71 @@ import { Row, Col,Button } from 'react-bootstrap/';
 import '../App.css';
 import { addTrendingSpots } from '../utils/parsingTools';
 import basicSearch from '../utils/eventsAPI';
-import Slider from '../components/Slider/Slider';
+
 import { Link } from 'react-router-dom';
 
 
-function EventsAndTrending(){
+function TrendingSpots({
+	request_id,
+	city,
+	latitude,
+	longitude
+}) {
+	const [trendingSpots, setTrendingSpots] = useState([]);
+	useEffect(() => {
+		async function fetchData() {
+			await fetch('http://localhost:4000/graphql', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					query: `{
+						imagesForRequestId(request_id:"${city}"){
+							image_id,
+                            image_link,
+                            geolocation,
+                            caption,
+							tag
+						}
+					}
+					`
+				})
+			})
+				.then((res) => res.json())
+				.then((res) => {
+					res.data.imagesForRequestId
+					&& res.data.imagesForRequestId.length !== 0
+					&& setTrendingSpots(res.data.imagesForRequestId);
+				});
+		}
+
+		fetchData();
+	}, city);
+
 	return (
 		<div>
-			<Slider/>
+			<div className="parallax">
+				<Row className="justify-content-center" style={{ paddingTop: '300px' }}>
+					<Row className="justify-content-center">
+						<Link
+							to={`/events?request_id=${request_id}&latitude=${latitude}&longitude=${longitude}`}
+						>
+							<Button>To Events</Button>
+						</Link>
+						<Col
+							style={{
+								backgroundColor: 'rgb(255, 255, 255)',
+								borderRadius: '20px'
+							}}
+							lg={8}
+						>
+							{addTrendingSpots(trendingSpots)}
+						</Col>
+					</Row>
+				</Row>
+				<footer id="footer" />
+			</div>
 		</div>
 	);
 }
 
-export default EventsAndTrending;
+export default TrendingSpots;
---
+++ client/src/pages/UserSelection.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import Slider from '../components/Slider/Slider';
+
+
+function EventsAndTrending(){
+	return (
+		<div>
+			<Slider/>
+		</div>
+	);
+}
+
+export default EventsAndTrending;
