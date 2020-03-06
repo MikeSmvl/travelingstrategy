bd667609e061d9c438d1bab558fb0dbc80044a61
steffan <steffan_v@hotmail.com>
2020-03-06 11:26:03 -0500

    #268 fetched latitude and longitute to redirect to respective city events

--- client/src/components/Slider/Slider.js
+++ client/src/components/Slider/Slider.js
@@ -1,19 +1,55 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import Carousel from 'react-bootstrap/Carousel'
+import { Link } from 'react-router-dom';
 import './Slider.css';
 import './Button.css'
 import image_1 from './pictures/Events.jpg'
 import image_2 from './pictures/Trending.jpg'
-import Carousel from 'react-bootstrap/Carousel'
 
 
-function ControlledCarousel() {
+function ControlledCarousel({
+	request_id,
+	city,
+	latitude,
+	longitude
+  }) {
   const [index, setIndex] = useState(0);
   const [direction, setDirection] = useState(null);
-
+  const [trendingSpots, setTrendingSpots] = useState([]);
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
     setDirection(e.direction);
   };
+
+  useEffect(() => {
+		async function fetchData() {
+			await fetch('http://localhost:4000/graphql', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					query: `{
+						imagesForRequestId(request_id:"${city}"){
+							image_id,
+                            image_link,
+                            geolocation,
+                            caption,
+							tag
+
+						}
+					}
+					`
+				})
+			})
+				.then((res) => res.json())
+				.then((res) => {
+					res.data.imagesForRequestId
+					&& res.data.imagesForRequestId.length !== 0
+					&& setTrendingSpots(res.data.imagesForRequestId);
+				});
+		}
+
+		fetchData();
+	}, city);
 
   return (
     <Carousel activeIndex={index} direction={direction} onSelect={handleSelect}>
@@ -23,7 +59,9 @@ function ControlledCarousel() {
             <img className="content-image" src={image_1} alt="slide-img" />
               <div className="content-details fadeIn-bottom">
                 <div className="button">
+                <Link to={`/events?request_id=${request_id}&latitude=${latitude}&longitude=${longitude}`} >
                   <span>Events</span>
+                </Link>
                 </div>
               </div>
         </div>
