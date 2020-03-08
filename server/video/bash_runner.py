a0783ef11f769402c740e58e30b2268e19a1b946
Mikael Samvelian <mikael.samvelian@gmail.com>
2020-03-07 22:51:30 -0500

    #278 Add unit test for the youtube helper class

--- server/video/bash_runner.py
+++ server/video/bash_runner.py
@@ -1,16 +1,17 @@
 import subprocess
-from os import path
 import json
 
-# Get relative path to the server/video folder
-base_dir = path.dirname(path.abspath(__file__))
+# Initialize empty videos dictionary that will be loaded with a json
 videos = {}
 
+# Load links gathered by country_video_links.py in the videos dictionary
 with open('video_links.json') as f:
     videos = json.load(f)
 
+# For every country and video link pair start a bash script as a subprocess
 try:
-    processes = [subprocess.Popen(["./youtube-gif.sh", "-c", c, "-l", v]) for c, v in videos.items() if c[0]=="C"]
+    processes = [subprocess.Popen(["./youtube-gif.sh", "-c", c, "-l", v]) for c, v in videos.items()]
+    # Wait for all processes to end
     for p in processes: p.wait()
 except Exception as error_msg:
     print(error_msg)
