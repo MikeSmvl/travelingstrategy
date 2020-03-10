88f282a90fd13e22463e4cbc3caec2d1a11de895
unknown <tdelaportas@hotmail.com>
2020-03-09 23:55:34 -0400

    #263 Added most of graphql function required to store evnts in db

--- server/src/resolvers/events.js
+++ server/src/resolvers/events.js
@@ -29,4 +29,65 @@ var eventsForRequest = {
 }
 
 
+var addEvents = {
+    type: graphql.GraphQLList(Event),
+    args: {
+        request_id: {
+            type: new graphql.GraphQLNonNull(graphql.GraphQLString)},
+        event_category: {
+            type: new graphql.GraphQLNonNull(graphql.GraphQLString)},
+        description: {
+             type: new graphql.GraphQLNonNull(graphql.GraphQLString)},
+        duration: {
+             type: new graphql.GraphQLNonNull(graphql.GraphQLString)},
+        start_date: {
+             type: new graphql.GraphQLNonNull(graphql.GraphQLString)},
+        end_date: {
+             type: new graphql.GraphQLNonNull(graphql.GraphQLString)},
+        title: {
+             type: new graphql.GraphQLNonNull(graphql.GraphQLString)},
+        labels: {
+             type: new graphql.GraphQLNonNull(graphql.GraphQLString)},
+        address: {
+             type: new graphql.GraphQLNonNull(graphql.GraphQLString)},
+        place_type: {
+             type: new graphql.GraphQLNonNull(graphql.GraphQLString)},
+        name_of_place: {
+            type: new graphql.GraphQLNonNull(graphql.GraphQLString)},
+
+
+        }
+    },
+    resolve: function(source, args, context) {
+      return new Promise((resolve, reject) => {
+        db.run(
+          `INSERT INTO chosen_events VALUES(?,?,?,?,?,?,?,?,?,?,?);`,
+          [args.request_id, args.event_category, ],
+          function(err) {
+            if (err) {
+              logger.error(err);
+              reject(err);
+            } else {
+              db.get(
+                'SELECT * FROM user ORDER BY user_id DESC LIMIT 1;',
+                (err, row) => {
+                  if (err) {
+                    logger.error(err);
+                    reject(err);
+                  }
+                  resolve({
+                    email: row['email'],
+                    password: row['password'],
+                    verified: row['verified']
+                  });
+                }
+              );
+            }
+          }
+        );
+      });
+    }
+  };
+
+
 module.exports = eventsForRequest
