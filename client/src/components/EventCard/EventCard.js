b8a49395e8d468016c504c467a13064f8b782ed4
unknown <tdelaportas@hotmail.com>
2020-03-10 04:45:23 -0400

    #263 #267 Added front-end function to store events

--- client/src/components/EventCard/EventCard.js
+++ client/src/components/EventCard/EventCard.js
@@ -4,6 +4,37 @@ import { Card as RBCard, Button } from 'react-bootstrap';
 import './EventCard.css';
 
 
+const handleSubmit = (event) => {
+	console.log("ahahahahahahahah");
+	addEvent();
+};
+
+
+async function addEvent() {
+	const aaa = `hello`
+	await fetch('http://localhost:4000/graphql', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/graphql' },
+		body: `mutation{
+				addEvents(request_id:"${aaa}", event_category:"${aaa}", description:"${aaa}", duration:"${aaa}", start_date:"${aaa}", end_date:"${aaa}", title:"${aaa}", labels:"${aaa}", address:"${aaa}", place_type:"${aaa}", name_of_place:"${aaa}")
+				{   request_id,
+					event_category,
+					description,
+					duration,
+					start_date,
+					end_date,
+					title,
+					labels,
+					address,
+					place_type,
+					name_of_place
+				} 
+			}`
+	});
+}
+
+
+
 const EventCard = (props) => {
 	const { children, title = '', subtitle = '', classExtra = '', ...rest } = props;
 	return (
@@ -11,7 +42,7 @@ const EventCard = (props) => {
 			{...rest}
 			className={`TScardBody ${classExtra}`}
 		>
-			<Button value="Add to Favorite" style={{float: 'left'}}>Add to Favorite</Button>
+			<Button value="Add to Favorite" style={{float: 'left'}} onClick={handleSubmit}>Add to Favorite</Button>
 
 			{title && <RBCard.Title>{title}</RBCard.Title>}
 			{subtitle && (
