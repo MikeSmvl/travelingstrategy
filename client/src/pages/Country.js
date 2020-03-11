26c81647e1f776cd14934d25e8f831fb5eb68441
Charles <chanc09@gmail.com>
2020-03-11 17:02:48 -0400

    #258 lint fix for image alt

--- client/src/pages/Country.js
+++ client/src/pages/Country.js
@@ -908,7 +908,7 @@ function Country({
 																	<img
 																		src={require('../miscImages/phraseSpeaker.png')}
 																		style={{ width: '24px' }}
-
+																		alt="error loading image"
 																	/>
 																</button>
 															</div>
