810c431efd4d23038d98643a14d2099ae86adee5
steffan <steffan_v@hotmail.com>
2020-03-17 17:42:11 -0400

    #257 changed syntax for fetching from graphql

--- client/src/components/Weather/WeatherGraph/WeatherGraph.js
+++ client/src/components/Weather/WeatherGraph/WeatherGraph.js
@@ -17,7 +17,7 @@ const WeatherGraph = (props) => {
 
   useEffect(() => {
     async function fetchData() {
-      await fetch(process.env.REACT_APP_BACKEND, {
+      await fetch(`${process.env.REACT_APP_BACKEND}graphql`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
