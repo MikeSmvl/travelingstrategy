a8c54fd441ff6fd3caa5f045206bb19141c04210
unknown <tdelaportas@hotmail.com>
2020-03-17 13:57:40 -0400

    #257 Added test for resolvers

--- server/src/__tests__/resolvers.queries.js
+++ server/src/__tests__/resolvers.queries.js
@@ -624,6 +624,73 @@ it("Querying events retrieved for a request_id", () =>{
     }
 }`;
 
+
+tester.test(true, query)
+tester.graphql(query, undefined, undefined, { isLocal: false })
+    .then(result => {
+      if(result.error != undefined){
+        logger.error(__filename +result.errors[0].message)
+      }
+      else{
+        logger.info(__filename +"There is no error in the query parameters")
+      }
+    })
+    .catch(err => logger.error(__filename +" "+err))
+});
+it("Querying events retrieved for a city", () =>{
+const query =`
+{
+  city_average_monthly_weather(city:"montreal"){
+    city,
+    january,
+    february,
+    march,
+    april,
+    may,
+    june,
+    july,
+    august,
+    septembre,
+    octobre,
+    novembre,
+    decembre,
+  }
+}`;
+
+
+tester.test(true, query)
+tester.graphql(query, undefined, undefined, { isLocal: false })
+    .then(result => {
+      if(result.error != undefined){
+        logger.error(__filename +result.errors[0].message)
+      }
+      else{
+        logger.info(__filename +"There is no error in the query parameters")
+      }
+    })
+    .catch(err => logger.error(__filename +" "+err))
+});
+it("Querying events retrieved for a city", () =>{
+const query =`
+{
+  monthly_weather_table{
+    city,
+    january,
+    february,
+    march,
+    april,
+    may,
+    june,
+    july,
+    august,
+    septembre,
+    octobre,
+    novembre,
+    decembre,
+  }
+}`;
+
+
   tester.test(true, query)
   tester.graphql(query, undefined, undefined, { isLocal: false })
       .then(result => {
@@ -658,6 +725,11 @@ describe("Test for add-subscriber mutation", () => {
     }
   );
 });
+
+
+
+
+
 
 // describe("Test to validate a user with an existing email cannot be created", () => {
 //   test("Should be an invalid mutation", () => {
