177718d87dc6a46114d53058ab5d19d644e21a1a
Mina <armirad@yahoo.ca>
2020-03-19 12:43:02 -0400

    #295 added the new Toast component to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import CountrySelector from '../components/CountrySelector/CountrySelector';
 import Hero from '../components/Hero/Hero';
+import { Row } from 'react-bootstrap'
+import Toast from '../../src/components/Toast/Toast';
+import { Link } from 'react-router-dom';
 
 function Home() {
-	return (
-		<div>
-			<Hero />
-			<CountrySelector originLabel="Origin" destinationLabel="Destination" />
-		</div>
-	);
+    return (
+        <div>
+            <Row className="justify-content-center">
+                <Toast type="alert" title="Covid-19" headerIcon="fa fa-notes-medical"><Link to="/covid19"><u>Click here</u> for information about the corona virus.</Link></Toast>
+            </Row>
+            <Hero />
+            <CountrySelector originLabel="Origin" destinationLabel="Destination" />
+        </div>
+    );
 }
 
 export default Home;
