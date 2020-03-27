ebabfe5046e3e5393e8e42928ccc44846399c20b
Oumar96 <oumarba221296@hotmail.fr>
2020-03-27 16:32:59 -0400

    #264 Adding password to key string

--- server/src/resolvers/emailEvents/sendEvents.py
+++ server/src/resolvers/emailEvents/sendEvents.py
@@ -4,19 +4,16 @@ import sys, json
 import keyring.backend
 from keyrings.alt.file import PlaintextKeyring
 
-keyring.set_keyring(PlaintextKeyring())
-
-recipientEmail = "oumarba221296@hotmail.fr" #Replace this by your email to until you're done working
-list_of_events = json.loads(sys.argv[2])
-
 #function constructs email dynamically with the number of events in the user's interest list
 def getEventsContent(list_of_events):
-
   email_html = Email_events_html()
-
   for events in list_of_events:
     email_html.add_events(events)
   return email_html.get_email().replace("\n", "")
+
+keyring.set_keyring(PlaintextKeyring())
+recipientEmail = "oumarba221296@hotmail.fr" #Replace this by your email to until you're done working
+list_of_events = json.loads(sys.argv[2])
 
 #piece all the elements of the email together
 email_construction = getEventsContent(list_of_events)
@@ -24,3 +21,5 @@ email_construction = getEventsContent(list_of_events)
 
 yag = yagmail.SMTP('noreply.travelingstrategy@gmail.com')
 yag.send(recipientEmail, 'TravelingStrategy', email_construction)
+
+
