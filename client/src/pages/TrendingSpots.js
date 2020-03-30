666435563fac38016a3b085dee9d746386f59045
Mina <armirad@yahoo.ca>
2020-03-30 03:12:08 -0400

    #247 styled Trending spots page, mostly done

--- client/src/pages/TrendingSpots.js
+++ client/src/pages/TrendingSpots.js
@@ -1,10 +1,20 @@
 import React, {useState, useEffect} from 'react';
-import {Row, Col} from 'react-bootstrap/';
+import {Row, Col, Jumbotron} from 'react-bootstrap/';
 import './TrendingSpots.css';
 import Frame from '../components/Frame/Frame';
 
 function TrendingSpots({requestId, city, latitude, longitude}) {
+	let curr = new Date();
+	let week = [];
+
+	for (let i = 1; i <= 7; i++) {
+		let first = (curr.getDate()) - curr.getDay() + i;
+		let day = new Date(curr.setDate(first)-604800000).toISOString().slice(0, 10);
+		week.push(day);
+	}
+	console.log('week', week);
 	const [trendingSpots, setTrendingSpots] = useState([]);
+
 	useEffect(() => {
 		async function fetchData() {
 			await fetch(`${process.env.REACT_APP_BACKEND}graphql`, {
@@ -14,10 +24,11 @@ function TrendingSpots({requestId, city, latitude, longitude}) {
 					query: `{
 						imagesForRequestId(request_id:"${requestId}"){
 							image_id,
-                            image_link,
-                            geolocation,
-                            caption,
-							tag
+              image_link,
+              geolocation,
+              caption,
+							tag,
+							date_retrieved
 						}
 					}
 					`
@@ -35,23 +46,42 @@ function TrendingSpots({requestId, city, latitude, longitude}) {
 	}, [city, requestId]);
 
 	return (
-		<>
-			{trendingSpots.map(image => {
-				//const city_in_url = citySubscription.search_term.toLowerCase().replace(' ', ''); // triming the city to match the tag
-				const image_url = image.image_link;
-				const geolocation = image.geolocation;
-				const caption = image.caption;
-				return (
-					<div>
-						<Frame
-							username={caption}
-							geolocation={geolocation}
-							img={image_url}
-						/>
-					</div>
-				);
-			})}
-		</>
+		<div className="wrapper">
+			<div style={{ textAlign: 'center', marginTop: '4%', fontWeight: '550', fontSize: '60px' }}>	Trending Spots in Bali</div>
+			<div
+				style={{
+					position: 'relative',
+					top: '2%',
+					display: 'grid',
+					gridTemplateColumns: '14% 14% 14% 14% 14%',
+					gridGap: '23px',
+					paddingBottom: '0',
+					width: '100%',
+					justifyContent: 'center',
+					alignItems: 'center'
+				}}>
+				{trendingSpots.map((image, idx) => {
+					//const city_in_url = citySubscription.search_term.toLowerCase().replace(' ', ''); // triming the city to match the tag
+					const image_url = image.image_link;
+					const geolocation = image.geolocation;
+					const caption = image.caption;
+					if (idx < 7) {
+						return (
+							<div>
+								<Frame
+									username={caption}
+									geolocation={geolocation}
+									img={image_url}
+									style={{width: '30px'}}
+								/>
+							</div>
+						);
+					}
+				})}
+				<div className='gridarea'>Daydream. We'll do the rest.</div>
+				<div className="date">2020/03/28</div>
+			</div>
+		</div>
 	);
 }
 
