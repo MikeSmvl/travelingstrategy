f4aeca1dad1574ac4c19be6b0f34007597177de1
LineG <ghanemline@gmail.com>
2020-03-31 12:11:43 -0400

    272 fixed the on mouse leave isssue making it more user friendly @svenaci

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -47,6 +47,7 @@ const Header = (props) => {
 							<button
 								onMouseOver={toggleWhy}
 								onFocus={toggleWhy}
+								onMouseLeave={toggleWhy}
 								className="iWhy"
 								type="button"
 							/>
