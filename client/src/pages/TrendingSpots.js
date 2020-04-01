638edea0a8cca883c4d3f4fddff6f303e5988b9a
Mina <armirad@yahoo.ca>
2020-04-01 00:08:38 -0400

    #247 made TrendingSpots page compatible for mobile and larger screens

--- client/src/pages/TrendingSpots.js
+++ client/src/pages/TrendingSpots.js
@@ -1,21 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import {Row, Col, Jumbotron} from 'react-bootstrap/';
 import './TrendingSpots.css';
 import Frame from '../components/Frame/Frame';
 
 function TrendingSpots({requestId, city, latitude, longitude}) {
-	let curr = new Date();
-	let week = [];
-
-	for (let i = 1; i <= 7; i++) {
-		let first = curr.getDate() - curr.getDay() + i;
-		let day = new Date(curr.setDate(first) - 604800000)
-			.toISOString()
-			.slice(0, 10);
-		week.push(day);
-	}
-	console.log('week', week);
 	const [trendingSpots, setTrendingSpots] = useState([]);
+	const [taggedCity, setTaggedCity] = useState('');
+	const [weekDate, setWeekDate] = useState('');
 
 	useEffect(() => {
 		async function fetchData() {
@@ -41,6 +31,8 @@ function TrendingSpots({requestId, city, latitude, longitude}) {
 					res.data.imagesForRequestId &&
 						res.data.imagesForRequestId.length !== 0 &&
 						setTrendingSpots(res.data.imagesForRequestId);
+					setTaggedCity(res.data.imagesForRequestId[0].tag);
+					setWeekDate(res.data.imagesForRequestId[0].date_retrieved);
 				});
 		}
 
@@ -49,27 +41,15 @@ function TrendingSpots({requestId, city, latitude, longitude}) {
 
 	return (
 		<div className='wrapper'>
-			<div className='mainTitle'> Trending Spots in Bali</div>
-			<div
-				style={{
-					position: 'relative',
-					top: '2%',
-					display: 'grid',
-					gridTemplateColumns: '14% 14% 14% 14% 14%',
-					gridGap: '23px',
-					paddingBottom: '0',
-					width: '100%',
-					justifyContent: 'center',
-					alignItems: 'center'
-				}}>
+			<div className='mainTitle'> Trending Spots in {taggedCity}</div>
+			<div className='CSSgrid'>
 				{trendingSpots.map((image, idx) => {
-					//const city_in_url = citySubscription.search_term.toLowerCase().replace(' ', ''); // triming the city to match the tag
 					const image_url = image.image_link;
 					const geolocation = image.geolocation;
 					const caption = image.caption;
 					if (idx < 7) {
 						return (
-							<div>
+							<div className="instaPhoto">
 								<Frame
 									username={caption}
 									geolocation={geolocation}
@@ -82,7 +62,7 @@ function TrendingSpots({requestId, city, latitude, longitude}) {
 				})}
 				<div className='quote'>Daydream. We'll do the rest.</div>
 				<div>
-					<div className='date'>2020/03/28</div>
+					<div className='weekDate'>{weekDate.replace('/', '-')}</div>
 					<div className='weekOf'>Week of </div>
 				</div>
 			</div>
