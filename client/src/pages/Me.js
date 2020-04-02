db204b7b27047c9f607ee32fcf96b9c1312c9a06
Mina <armirad@yahoo.ca>
2020-04-01 21:27:06 -0400

    #247 styled the Me.js page as mockup

--- client/src/pages/Me.js
+++ client/src/pages/Me.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Redirect, Link } from 'react-router-dom';
-import { CitiesCard, CityImage } from '../components/CitiesCard/CitiesCard';
+import React, {useState, useEffect} from 'react';
+import {Redirect, Link} from 'react-router-dom';
+import {CitiesCard, CityImage} from '../components/CitiesCard/CitiesCard';
+import TStimbre from './TStimbre.png'
 
 import './Me.css';
 
@@ -8,17 +9,18 @@ function Me() {
 	const [redirect, setRedirect] = useState(false);
 	const [email, setEmail] = useState('');
 	const [cities, setCities] = useState([]);
+	const currentDate = new Date().toISOString().slice(0, 10);
 
 	useEffect(() => {
 		async function getToken() {
 			await fetch(`${process.env.REACT_APP_BACKEND}checktoken`, {
 				credentials: 'include'
 			})
-				.then((res) => res.json())
-				.then((res) => {
+				.then(res => res.json())
+				.then(res => {
 					res.email && res.email !== null && setEmail(res.email);
 				})
-				.catch((error) => {
+				.catch(error => {
 					// if status code 401...
 					setRedirect(true);
 				});
@@ -27,7 +29,7 @@ function Me() {
 		async function fetchData() {
 			await fetch(`${process.env.REACT_APP_BACKEND}graphql`, {
 				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
+				headers: {'Content-Type': 'application/json'},
 				body: JSON.stringify({
 					query: `{
 						userSubscriptions(email:"${email}"){
@@ -42,11 +44,11 @@ function Me() {
 					`
 				})
 			})
-				.then((res) => res.json())
-				.then((res) => {
-					res.data.userSubscriptions
-						&& res.data.userSubscriptions.length !== 0
-						&& setCities(res.data.userSubscriptions);
+				.then(res => res.json())
+				.then(res => {
+					res.data.userSubscriptions &&
+						res.data.userSubscriptions.length !== 0 &&
+						setCities(res.data.userSubscriptions);
 				});
 		}
 
@@ -55,29 +57,33 @@ function Me() {
 	}, [email]);
 
 	if (redirect) {
-		return <Redirect to="/" />;
+		return <Redirect to='/' />;
 	}
 
 	return (
-		<div style={{ display: 'flex', justifyContent: 'center', marginTop: '12%' }}>
-			{cities.map((citySubscription, idx) => {
-				const requestId = citySubscription.request_id;
-				const cityName = citySubscription.search_term;
-				const { latitude } = citySubscription;
-				const { longitude } = citySubscription;
+		<>
+			<div className='currentDate'>{currentDate.replace('/', '-')}</div>
+			<div className='meTitle'>My Places</div>
+			<div className='citiesCardWrapper'>
+				{cities.map((citySubscription, idx) => {
+					const requestId = citySubscription.request_id;
+					const cityName = citySubscription.search_term;
+					const {latitude} = citySubscription;
+					const {longitude} = citySubscription;
 
-				return (
-					<Link
-						key={idx}
-						to={`/user_selection?request_id=${requestId}&city=${cityName}&latitude=${latitude}&longitude=${longitude}`}
-					>
-						<CitiesCard>
-							<CityImage key={idx} cityName={cityName.toLowerCase()} />
-						</CitiesCard>
-					</Link>
-				);
-			})}
-		</div>
+					return (
+						<Link
+							key={idx}
+							to={`/user_selection?request_id=${requestId}&city=${cityName}&latitude=${latitude}&longitude=${longitude}`}>
+							<CitiesCard>
+								<CityImage key={idx} cityName={cityName.toLowerCase()} />
+							</CitiesCard>
+						</Link>
+					);
+				})}
+			</div>
+			<img className="timbreImage" alt="" src={TStimbre} />
+		</>
 	);
 }
 
