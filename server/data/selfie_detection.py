04af4541be2242a480f348adf081b819d3206025
Mina <armirad@yahoo.ca>
2020-04-12 17:40:24 -0400

    Made main function with threshold to identify selfie, and highlight the face to be stored in seperate folder

--- server/data/selfie_detection.py
+++ server/data/selfie_detection.py
@@ -23,3 +23,33 @@ def search_for_faces(image_path):
     # each face is represented as a list of characteristics that are in the form of 'key': value pairs
     faces = algorithm.detect_faces(image)
     return faces
+
+
+# Main function that determines if photo is a selfie
+def check_if_selfie(image_path, faces):
+    # reads the image
+    image = pyplot.imread(image_path)
+    # plots the image with pyplot
+    pyplot.imshow(image)
+    # use pyplot's gca method for the axis system
+    img_with_axes = pyplot.gca()
+    # get dimensions of original image
+    og_width, og_height = PIL.Image.open('images_to_filter/test2.jpg').size
+    # draw a rectangle for each face based on the coordinates and save the photo in Discarded/
+    for face in faces:
+        # get dimensions of the face
+        x, y, face_width, face_height = face['box']
+        if face_width/og_width > 0.25 or face_height/og_height > 0.22:
+            # outline the face with a rectangle
+            face_rectangle = Rectangle((x, y), face_width, face_height,
+                                       fill=False, color='purple')
+            # add the rectangle to the image with the axes
+            img_with_axes.add_patch(face_rectangle)
+            pyplot.savefig('images_to_filter/discarded/selfie.jpg')
+            return True
+    return False
+
+
+save_img_url('https://instagram.fymy1-2.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/91884994_131277001783285_8536432134989860630_n.jpg?_nc_ht=instagram.fymy1-2.fna.fbcdn.net&_nc_cat=101&_nc_ohc=ctzUWQ6BM68AX9i_i8W&oh=c49d5152440d702185d73b572493cb83&oe=5EAB64C7', 'images_to_filter/test2.jpg')
+faces = search_for_faces('images_to_filter/test2.jpg')
+print(check_if_selfie('images_to_filter/test2.jpg', faces))
